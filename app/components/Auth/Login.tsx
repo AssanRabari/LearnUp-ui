d312faa66d39fa9477a0c9a81d2aad2204301cc4
AssanRabari <assandesai@gmail.com>
2024-08-11 10:27:32 +0530

    login modal basic design and formik setup

--- app/components/Auth/Login.tsx
+++ app/components/Auth/Login.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+'user-client';
 
-type Props = {}
+import React, { FC, useState } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup';
+import { AiOutlineEye, AiOutlineEyeInvisible, AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import { styles } from '../../styles/style';
 
-const Login = (props: Props) => {
+type Props = { setRoute: (route: string) => void }
+
+const schema = Yup.object().shape({
+    email: Yup.string().email("Invalid email").required("Please enter your email"),
+    password: Yup.string().required("please enter your password").min(6)
+})
+
+const Login: FC<Props> = ({ setRoute }) => {
+    const [show, setShow] = useState(false)
+
+    const formik = useFormik({
+        initialValues: { email: "", password: "" },
+        validationSchema: schema,
+        onSubmit: async ({ email, password }) => { console.log(email, password) }
+    });
+
+    const { errors, touched, values, handleChange, handleSubmit } = formik;
+
     return (
-        <div>Login</div>
+        <div className='w-full'><div className={styles.title}>Login with LearnUp</div></div>
     )
 }
 
