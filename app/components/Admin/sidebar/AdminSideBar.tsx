572055bd5b3da8adf917eb409f515442067c939b
Assan Rabari <assandesai@gmail.com>
2024-12-24 23:19:38 +0530

    Admin Sidebar basic component

--- app/components/Admin/sidebar/AdminSideBar.tsx
+++ app/components/Admin/sidebar/AdminSideBar.tsx
@@ -1,23 +1,9 @@
 import React from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from "react-pro-sidebar";
+
 type Props = {};
 
 function AdminSideBar({}: Props) {
-  return (
-    <div>
-      <Sidebar>
-        <Menu>
-          <SubMenu label="Charts">
-            <MenuItem> Pie charts </MenuItem>
-            <MenuItem> Line charts </MenuItem>
-          </SubMenu>
-          <MenuItem> Documentation </MenuItem>
-          <MenuItem> Calendar </MenuItem>
-        </Menu>
-      </Sidebar>
-      ;
-    </div>
-  );
+  return <div>AdminSideBar</div>;
 }
 
 export default AdminSideBar;
